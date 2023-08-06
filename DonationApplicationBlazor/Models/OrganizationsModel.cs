0b39fb38eb0f06b1a4ca5a94cdc69d6b734472ae
ahbab259 <hasin_ahbab@yahoo.com>
2023-08-06 11:18:17 -0400

    Model Separation

---
+++ DonationApplicationBlazor/Models/OrganizationsModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DonationApplicationBlazor.Models
+{
+    public class OrganizationsModel
+    {
+        public int Id { get; set; }
+        public string OrganizationName { get; set; }
+        public string OrganizationType { get; set; }
+        public string OrganizationDescription { get; set; }
+        public string OrganizationCountryName { get; set; }
+        public string OrganizationCountryCode { get; set; }
+        public string OrganizationEmail { get; set; }
+        public string OrganizationPhone { get; set; }
+        public string OrganizationCode { get; set; }
+    }
+}
